e64ebd420a58ab63c422e61780ce5f4847c59258
KasperKolasinski <kasper.kolasinski@gmail.com>
2023-01-26 17:13:05 -0500

    Added Stretch Requirements Pt.1

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,11 +15,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button_adder)
+        val upgradeBtn = findViewById<Button>(R.id.upgradeBtn)
         val textView = findViewById<TextView>(R.id.textView)
         button.setOnClickListener {
             //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
             counter++
             textView.text = counter.toString()
+
+            if (counter >= 100) {
+                upgradeBtn.visibility = View.VISIBLE
+                upgradeBtn.setOnClickListener {
+                    button.text = "Add 2"
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+                    upgradeBtn.visibility = View.INVISIBLE
+                }
+            }
         }
     }
 }
