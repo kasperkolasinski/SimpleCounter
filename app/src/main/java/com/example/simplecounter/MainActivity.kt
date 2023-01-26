d10fcf947bd771891d6dddb213083d56b1eee31c
KasperKolasinski <kasper.kolasinski@gmail.com>
2023-01-26 16:40:13 -0500

    Basic Requirements Completed

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.simplecounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button_adder)
-
+        val textView = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+        }
     }
 }
